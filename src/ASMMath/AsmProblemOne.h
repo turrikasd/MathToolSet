f43ed755194402e07c3e85dd5959016bb58160f7
Despite <despite.mn@gmail.com>
2013-03-29 04:12:03 +0200

    *nix style shared library exporting support

--- src/ASMMath/AsmProblemOne.h
+++ src/ASMMath/AsmProblemOne.h
@@ -1,10 +1,14 @@
-#ifdef ASMMath_EXPORTS
-#define ASMMath_EI __declspec(dllexport)
-#else
-#define ASMMath_EI __declspec(dllimport)
+#if defined (__unix__)
+	#define ASMMath_EI __attribute__ ((__visibility__("default")))
+#elif defined (WIN32)
+	#ifdef ASMMath_EXPORTS
+		#define ASMMath_EI __declspec(dllexport)
+	#else
+		#define ASMMath_EI __declspec(dllimport)
+	#endif
 #endif
 
 #include <iostream>
 
-void ASMMath_EI AsmProblemOne();
-void PrintOut(int num);
+extern void ASMMath_EI AsmProblemOne();
+extern void PrintOut(int num);
--- src/CPPMath/CppProblemTwo.h
+++ src/CPPMath/CppProblemTwo.h
@@ -1,9 +1,13 @@
-#ifdef CPPMath_EXPORTS
-#define CPPMath_EI __declspec(dllexport)
-#else
-#define CPPMath_EI __declspec(dllimport)
+#if defined (__unix__)
+#define CPPMath_EI __attribute__ ((__visibility__("default")))
+#elif defined (WIN32)
+	#ifdef CPPMath_EXPORTS
+		#define CPPMath_EI __declspec(dllexport)
+	#else
+		#define CPPMath_EI __declspec(dllimport)
+	#endif
 #endif
 
 #include <iostream>
 
-void CPPMath_EI CppProblemTwo();
+extern void CPPMath_EI CppProblemTwo();
